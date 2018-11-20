b8d20669a2785cfc2143e5e10a3bed7d5a86c22f
Kevin Y. Chen <kchen@palantir.com>
2018-11-19 21:07:10 -0800

    Add xcode projects

---
+++ readme.md
@@ -0,0 +1,5 @@
+Visi
+====
+
+To start development, open `visi.xcworkspace` in XCode. This project was developed with XCode 9.4.1 on OSX 10.13.6.
+
---
+++ visi-core/visi-core/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  visi-core
+//
+//  Created by Kevin Chen on 11/19/18.
+//  Copyright © 2018 Kevin Chen. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}

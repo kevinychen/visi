1f1bd93ddbb1c9b9066a582bed31e98a3874a564
Kevin Y. Chen <kchen@palantir.com>
2018-11-19 22:10:04 -0800

    Add OpenCV

--- readme.md
+++ readme.md
@@ -3,3 +3,5 @@ Visi
 
 To start development, open `visi.xcworkspace` in XCode. This project was developed with XCode 9.4.1 on OSX 10.13.6.
 
+Install OpenCV with `brew install opencv`. Then run `pkg-config --cflags --libs opencv` and replace the "Header Search Paths" and "Library Search Paths" in the `visi-core` project with the appropriate paths to your installation.
+
--- visi-core/visi-core/main.cpp
+++ visi-core/visi-core/main.cpp
@@ -6,10 +6,23 @@
 //  Copyright © 2018 Kevin Chen. All rights reserved.
 //
 
-#include <iostream>
+#include "opencv2/opencv.hpp"
+#include "opencv2/highgui/highgui.hpp"
+
+using namespace cv;
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    Mat output = Mat::zeros(120, 350, CV_8UC3);
+    putText(output,
+            "Hello world!",
+            cvPoint(15, 70), /* origin */
+            FONT_HERSHEY_PLAIN,
+            3., /* font scale */
+            cvScalar(0, 255, 0),
+            4 /* line type */);
+
+    namedWindow("output");
+    imshow("output", output);
+    waitKey(0);
     return 0;
 }
